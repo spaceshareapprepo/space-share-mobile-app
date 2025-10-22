f219472a3fb5176b10165b9c7637052ba5a09766
OfosuA <alfred.ofosu@ec.gc.ca>
2025-10-22 19:14:29 -0400

    add supabe login auth

---
+++ lib/supabase.ts
@@ -0,0 +1,28 @@
+import { AppState, Platform } from "react-native";
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient, processLock } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL,
+  process.env.EXPO_PUBLIC_SUPABASE_KEY,
+  {
+    auth: {
+      ...(Platform.OS === "web" ? {} : { storage: AsyncStorage }),
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+      lock: processLock,
+    },
+  }
+);
+
+if (Platform.OS !== "web") {
+  AppState.addEventListener("change", (state) => {
+    if (state === "active") {
+      supabase.auth.startAutoRefresh();
+    } else {
+      supabase.auth.stopAutoRefresh();
+    }
+  });
+}
